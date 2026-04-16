deed7cfff6b986d021126f093e1487d7a41d742a
rahman <rahman.jazlan@gmail.com>
2026-04-16 18:54:47 +0800

    enemy spawn kinda done

--- Scripts/enemy_test.gd
+++ Scripts/enemy_test.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+@export var speed : float = 100.0
+
+
+func _physics_process(delta: float) -> void:                    
+	var direction = Vector2.ZERO                                   														   
+	var player = get_node_or_null("/root/main/Player1")            
+	if player:                                                     
+		direction = (player.global_position - global_position).normalized() 															 
+		velocity = direction * speed                                   
+		move_and_slide()   
+
+
+#func _process(delta: float) -> void:
+	#position.y -= speed * delta
