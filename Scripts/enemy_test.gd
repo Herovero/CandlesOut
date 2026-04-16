ad0249a7c19b368829c2e927757b19d36a8a43b4
Herovero <herovero0117@gmail.com>
2026-04-16 18:07:21 +0800

    Enemy follow player 1 or 2 based on nearest position

--- Scripts/enemy_test.gd
+++ Scripts/enemy_test.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: float = 150.0
+const speed: float = 100.0
+const hp: float = 100.0
 @export var damage_amount: float = 1.0
 
 @onready var hitbox: Area2D = $Hitbox
@@ -10,6 +11,33 @@ var target_player: CharacterBody2D = null
 
 func _ready():
 	pass
+
+func _physics_process(delta: float) -> void:
+	var target = find_closest_player()
+	var direction = Vector2.ZERO
+	
+	if target:
+		direction = global_position.direction_to(target.global_position)
+
+	velocity = direction * speed
+	move_and_slide()
+
+func find_closest_player() -> CharacterBody2D:
+	var players = get_tree().get_nodes_in_group("Players") # Matches your group name
+	var closest_player = null
+	var shortest_distance = INF # Start with infinity
+	
+	for player in players:
+		# Avoid targeting the 'Ghost' if it's in the same group but shouldn't be chased
+		if player.has_method("is_sleeping") and player.is_sleeping:
+			continue
+			
+		var distance = global_position.distance_to(player.global_position)
+		if distance < shortest_distance:
+			shortest_distance = distance
+			closest_player = player
+			
+	return closest_player
 	
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Players"):
