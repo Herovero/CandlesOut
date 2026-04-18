a1d7d38e5d09f1657679eef220f1398edc100a6f
Herovero <herovero0117@gmail.com>
2026-04-19 00:36:03 +0800

    Improved melee enemy attack hitbox

--- Scripts/Spawners/wave_manager.gd
+++ Scripts/Spawners/wave_manager.gd
@@ -8,12 +8,12 @@ signal wave_completed(wave_number : int)
 
 const WAVE_DATA = {
 	1: [
-		{"scene": preload("res://Scenes/enemy_test.tscn"), "count": 8},
+		{"scene": preload("res://Scenes/enemy_test.tscn"), "count": 6},
 		{"scene": preload("res://Scenes/enemy_ranged.tscn"), "count": 0},
 		{"scene": preload("res://Scenes/enemy_boss.tscn"), "count": 0}
 		],
 	2: [
-		{"scene": preload("res://Scenes/enemy_ranged.tscn"), "count": 2},
+		{"scene": preload("res://Scenes/enemy_ranged.tscn"), "count": 4},
 		{"scene": preload("res://Scenes/enemy_test.tscn"), "count": 4},
 		],
 	3: [
